4ec5927358a9a2602b5e0f6d73ee7a15ff5b415c
Ayan Atal <ayanatal.me19@rvce.edu.in>
2020-09-04 14:21:19 +0530

    Updates

---
+++ precache-manifest.67c527a0dabcff05464234e1795b5cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64f0a46ae9c42e0df21dc24f89a8fe8",
+    "url": "/color-changer/index.html"
+  },
+  {
+    "revision": "229a1d6bdfe6e4f2ef29",
+    "url": "/color-changer/static/css/main.db51de83.chunk.css"
+  },
+  {
+    "revision": "e650b66672d110c8a6aa",
+    "url": "/color-changer/static/js/2.2e3da848.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-changer/static/js/2.2e3da848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229a1d6bdfe6e4f2ef29",
+    "url": "/color-changer/static/js/main.d943731f.chunk.js"
+  },
+  {
+    "revision": "fe5d89ecc88950b2eac8",
+    "url": "/color-changer/static/js/runtime-main.2bf0bacd.js"
+  }
+]);
